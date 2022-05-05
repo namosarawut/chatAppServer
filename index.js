4981ee4204ec7b625690c0d44bc42486292f5de9
Sarawut Saengchan <namosarawut02@gmail.com>
2022-05-06 04:40:11 +0700

    heroku first deployment

--- index.js
+++ index.js
@@ -24,34 +24,14 @@ io.on("connection",(socket)=>{
     if (clients[targetId]) clients[targetId].emit("message", msg);
   });
 });
-// "message",
-// {"message": message, "sourceid": sourceID, "targetid": targetID},
+app.route("/check").get((req,res)=>{
+  return res.json("Chat Server is Working!!")
+})
+
 server.listen(port,"0.0.0.0",()=>{
     console.log("server started!! "+ port)
 })
 
 
-
-// app.use(express.json());
-// var clients = {};
-
-// io.on("connection", (socket) => {
-//   console.log("connetetd");
-//   console.log(socket.id, "has joined");
-//   socket.on("signin", (id) => {
-//     console.log(id);
-//     clients[id] = socket;
-//     console.log(clients);
-//   });
-//   socket.on("message", (msg) => {
-//     console.log(msg);
-//     let targetId = msg.targetId;
-//     if (clients[targetId]) clients[targetId].emit("message", msg);
-//   });
-// });
-
-// server.listen(port, "0.0.0.0", () => {
-//   console.log("server started");
-// });
 
 
